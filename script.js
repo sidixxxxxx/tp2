ecdb213afca7c1e1155c3e3d27272392c2db759a
sidi <sidimoh.mouta@gmail.com>
2025-02-04 14:21:14 +0100

    deuxieme commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function afficherMessage() {
     document.getElementById('message').innerText = 'Bravo, tu as cliqué sur le bouton !';
 }
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello, my name is Alla Jammine!';
